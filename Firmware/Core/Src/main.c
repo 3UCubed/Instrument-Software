d4e132dcfdf4cb2d4630d4ba0d3d143c60e0a4c9
Jared Desktop <jck1044@wildcats.unh.edu>
2023-06-20 10:33:38 -0400

    change baud rate back to 115200. Previous commit was 9600.

--- Firmware/Core/Src/main.c
+++ Firmware/Core/Src/main.c
@@ -369,8 +369,8 @@ void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim) {
 
 
 					if (HK_ON) {
-						HAL_UART_Transmit(&huart1, temps_buf, sizeof(temps_buf), 100);
-						HAL_UART_Transmit(&huart1, hk_buf, sizeof(hk_buf), HAL_MAX_DELAY);
+						HAL_UART_Transmit(&huart1, temps_buf, sizeof(temps_buf), HAL_MAX_DELAY);
+						HAL_UART_Transmit(&huart1, hk_buf, sizeof(hk_buf), 100);
 
 					}
 					hk_counter = 1;
@@ -1148,7 +1148,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 9600;
+  huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
