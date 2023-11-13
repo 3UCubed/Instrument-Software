d03fc9cf3a3369c656a33aab61f6b164214b9a23
mehtasanj <sanjeev.mehta@unh.edu>
2023-11-13 16:20:16 -0500

    uncommented HAL_UART_Receive_IT in rx_callback function and the main while loop; enabled PB5–SYS_ON in ioc

--- Test-Firmware/Core/Src/main.c
+++ Test-Firmware/Core/Src/main.c
@@ -671,9 +671,7 @@ int main(void)
   MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
-  for (int i = 0; i < (sizeof(gpios) / sizeof(gpios[0])); i++) {
-    	  HAL_GPIO_WritePin(gpios[i].gpio, gpios[i].pin, GPIO_PIN_RESET);
-    }
+
 
   HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
 
@@ -1350,7 +1348,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOF, GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PC13 PC6 PC7 PC8
                            PC9 PC10 */
@@ -1368,8 +1366,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PB6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_6;
+  /*Configure GPIO pins : PB5 PB6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
